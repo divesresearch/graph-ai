cd88acaa613012b04a8fbfea65729202c455b825
Ibrahim Karakoc <hibrahimkarakoc@outlook.com>
2023-02-18 16:23:39 +0100

    Implement the functions for The Graph request and parser

---
+++ utils/the_graph.py
@@ -0,0 +1,15 @@
+import requests
+from flatten_json import flatten
+
+def post_query(url, query):
+    response = requests.post(url, '', json={'query':query})
+    if response.status_code == 200 and 'data' in response.json():
+        return response.json()
+    else:
+        print(response.json())
+
+def parse_results(results):
+    new_results = []
+    for result in results:
+        new_results.append(flatten(result))
+    return new_results
