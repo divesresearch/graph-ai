ba2a6d693bd16f882bf7d706561cadbbd551998f
Ibrahim Karakoc <hibrahimkarakoc@outlook.com>
2023-02-18 16:39:14 +0100

    Implement the basic flow of the app

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
 import streamlit as st
 from revChatGPT.V1 import Chatbot
-
+import pandas as pd
+from utils.the_graph import post_query, parse_results
+from utils.prompts import protocol_selection_prompt, query_prompt
+from utils.urls import URLS
+from utils.schemas import SCHEMAS
 
 st.set_page_config(layout="wide")
 access_token = st.secrets['chatbot']['TOKEN']
 
-chatbot = Chatbot(config={
-  "access_token": access_token
-})
+user_input = st.text_input("What do you want?")
+
+if user_input:
+    chatbot = Chatbot(config={
+        "access_token": access_token
+    })
+    with st.spinner(text='Detecting the protocol...'):
+        protocol = ''
+        prev_text = ""
+        for data in chatbot.ask(
+            protocol_selection_prompt%(user_input)
+        ):
+            message = data["message"][len(prev_text) :]
+            protocol += message
+            prev_text = data["message"]
+
+    schema = SCHEMAS[protocol]
+
+    chatbot = Chatbot(config={
+        "access_token": access_token
+    })
+    with st.spinner(text='Writing the query...'):
+        response = ''
+        prev_text = ""
+        for data in chatbot.ask(
+            query_prompt%(protocol, user_input, schema)
+        ):
+            message = data["message"][len(prev_text):]
+            response += message
+            prev_text = data["message"]
+
+    with st.spinner(text='Sending the request...'):
+        results = post_query(URLS[protocol], response.strip('```'))
+        for key, value in results['data'].items():
+            df = pd.DataFrame(parse_results(value))
+
+    st.dataframe(df)
