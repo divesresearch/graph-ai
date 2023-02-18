269e26da9a7a77e7ef124780307b30954f6c07b3
Ibrahim Karakoc <hibrahimkarakoc@outlook.com>
2023-02-18 17:12:31 +0100

    Add new subgraphs

--- utils/urls.py
+++ utils/urls.py
@@ -1,3 +1,6 @@
 URLS = {
-    'uniswap': 'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-ethereum'
+    'uniswap': 'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-ethereum',
+    'balancer': 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer',
+    'usdc': 'https://api.thegraph.com/subgraphs/name/centrehq/usdc',
+    'decentraland': 'https://api.thegraph.com/subgraphs/name/decentraland/marketplace',
 }
