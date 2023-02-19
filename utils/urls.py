7d6a3b86c10886bad4ebc4bea215e6077d8e53e5
Giray <giraydemirell@gmail.com>
2023-02-19 15:51:10 +0300

    Add Aave URL and schema. Changed prompt

--- utils/urls.py
+++ utils/urls.py
@@ -1,6 +1,7 @@
 URLS = {
     'uniswap': 'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-ethereum',
     'balancer': 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer',
-    'usdc': 'https://api.thegraph.com/subgraphs/name/centrehq/usdc',
     'decentraland': 'https://api.thegraph.com/subgraphs/name/decentraland/marketplace',
+    'opensea' : 'https://api.thegraph.com/subgraphs/name/messari/opensea-v2-ethereum',
+    'aave' : 'https://api.thegraph.com/subgraphs/name/aave/protocol-v3'
 }
